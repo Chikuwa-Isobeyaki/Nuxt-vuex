53eeb7b0ce92703aef47970ba00b89a280df8740
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-08-17 21:41:09 +0900

    [Add]アクションの追加

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,9 @@
   <section class="container">
     <div>
       <p>{{ $store.state.message }}</p>
-      <button v-on:click="$store.commit('updateMessage', 'Commit with payload')">Update</button>
+      <!-- <button v-on:click="$store.commit('updateMessage', 'Commit with payload')">Update</button> -->
+      <!-- <button v-on:click="$store.dispatch('updateMessageAction')">dispatch</button> -->
+      <button v-on:click="$store.dispatch('updateMessageAction', 'Dispatch with payload')">dispatch</button>
     </div>
   </section>
 </template>
